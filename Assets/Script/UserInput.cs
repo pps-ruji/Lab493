2e84e4a308bf5ba269c987171ef974205546a0c6
pps-ruji <145337876+pps-ruji@users.noreply.github.com>
2023-11-04 03:46:53 +0700

    ีUpdatehalfGame

--- Assets/Script/UserInput.cs
+++ Assets/Script/UserInput.cs
@@ -5,38 +5,15 @@ using System.Linq;
 
 public class UserInput : MonoBehaviour
 {
-    public GameObject slot1;
     private Solitaire solitaire;
-    private float timer;
-    private float doubleClickTime = 0.3f;
-    private int clickCount = 0;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         solitaire = FindObjectOfType<Solitaire>();
-        slot1 = this.gameObject;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (clickCount == 1)
-        {
-            timer += Time.deltaTime;
-        }
-        if (clickCount == 3)
-        {
-            timer = 0;
-            clickCount = 1;
-        }
-        if (timer > doubleClickTime)
-        {
-            timer = 0;
-            clickCount = 0;
-        }
-
         GetMouseClick();
     }
 
@@ -44,8 +21,6 @@ public class UserInput : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            clickCount++;
-
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -10));
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
             if (hit)
