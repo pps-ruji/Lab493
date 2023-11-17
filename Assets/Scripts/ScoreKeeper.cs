92a2f5f3e6715d46bca2388304e5b7d84a4ac72f
pps-ruji <145337876+pps-ruji@users.noreply.github.com>
2023-11-17 14:02:59 +0700

    Update-4

---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    public Selectable[] topStacks;
+    public GameObject highScorePanel;
+
+    void Update()
+    {
+        if (HasWon())
+        {
+            Win();
+        }
+    }
+
+    public bool HasWon()
+
+    {
+        int i = 0;
+        foreach (Selectable topstack in topStacks)
+        {
+            i += topstack.value;
+        }
+        if (i >= 52)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    void Win()
+    {
+        highScorePanel.SetActive(true);
+        print("You have won!");
+    }
+}
---
+++ Assets/Scripts/UIButtons.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButtons : MonoBehaviour
+{
+    public GameObject highScorePanel;
+
+    public void PlayAgain()
+    {
+        highScorePanel.SetActive(false);
+        ResetScene();
+    }
+
+    public void ResetScene()
+    {
+        UpdateSprite[] cards = FindObjectsOfType<UpdateSprite>();
+        foreach (UpdateSprite card in cards)
+        {
+            Destroy(card.gameObject);
+        }
+        ClearTopValues();
+        FindObjectOfType<Solitaire>().PlayCards();
+    }
+
+    void ClearTopValues()
+    {
+        Selectable[] selectables = FindObjectsOfType<Selectable>();
+        foreach (Selectable selectable in selectables)
+        {
+            if (selectable.CompareTag("Top"))
+            {
+                selectable.suit = null;
+                selectable.value = 0;
+            }
+        }
+    }
+}
